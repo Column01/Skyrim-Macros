9ea1b74d2da5c7b9704878dba13822dda374437f
Column01 <colinandress999@gmail.com>
2021-01-18 14:09:13 -0500

    Change auto conjuration to generic auto cast
    Changed the auto conjuration script to be a generic auto cast macro.
    Also added common ways to level using it to the readme

---
+++ Auto Cast/README.md
@@ -0,0 +1,17 @@
+# Auto Cast
+
+Use this script to automatically cast a spell repeatedly for leveling that school of magic.
+
+## Common uses
+
+### Conjuration
+
+- Kill something living (animal, person, etc.) and move it to a safe location
+- Equip the "Soul Trap" spell and then use this to cast it on the dead thing over and over for fast conjuration leveling.
+
+NOTE: USLEEP **must** be disabled in order for this to work as it patches it! (**U**nofficial **S**kyrim **Le**gendary **E**dition **P**atch)
+
+### Illusion
+
+- Cast the "Courage" spell on the Talos preacher (Heimskr) in Whiterun until lvl 30 or so
+- Then cast "Muffle" while standing in Dragon's Reach until lvl 100
--- Auto Cast/auto_cast.py
+++ Auto Cast/auto_cast.py
@@ -4,7 +4,7 @@ import time
 
 
 mouse = Controller()
-print("Welcome! This script will help you automatically shoot a corpse with soul trap to train conjuration."
+print("Welcome! This script will help you automatically cast a spell that requires charging up to use."
       "\nThis will also calculate for optimal casting so be sure to enter the correct values.")
 
 
@@ -16,11 +16,11 @@ def initialize_magicka():
     return mag
 
 
-def initialize_soultrap_cost():
-    cost = int(input('What does it cost for you to cast the soultrap spell?: '))
+def initialize_spell_cost():
+    cost = int(input('What does it cost for you to cast the spell?: '))
     while cost <= 0:
-        print('Please input a value above zero for the cost of the soultrap spell.')
-        cost = int(input('How much magicka does it cost for you to cast the soultrap spell?: '))
+        print('Please input a value above zero for the cost of the spell.')
+        cost = int(input('How much magicka does it cost for you to cast the spell?: '))
     return cost
 
 
@@ -35,7 +35,7 @@ def initialize_wait_amount():
 try:
     # Initialize the variables needed to calculate casts and whatnot.
     magicka = initialize_magicka()
-    soultrap_cost = initialize_soultrap_cost()
+    spell_cost = initialize_spell_cost()
     num_waits = initialize_wait_amount()
 except KeyboardInterrupt:
     print("Quitting due to keyboard interrupt.")
@@ -43,32 +43,31 @@ except KeyboardInterrupt:
 
 
 # Not enough magicka to cast the spell
-if magicka < soultrap_cost:
+if magicka < spell_cost:
     exit('You do not have enough magicka to cast the spell.'
          '\nPlease make sure you level up your character some or clear and debuffs that affect magicka before proceeding.')
 
 # Number of times to cast the spell per wait command.
-casts_per_wait = int(magicka / soultrap_cost)
+casts_per_wait = int(magicka / spell_cost)
 time_per_wait = (1.25 * casts_per_wait) + 2.1
 
 mins, secs = divmod((time_per_wait * num_waits), 60)
 print("Estimated time required: {} minute(s) and {} second(s)\n".format(int(mins), int(secs)))
 
 print("Great! Now it's time to setup for training. "
-      "Please kill a mob (anything \"living\" works. A deer, a wolf, anything) and equip the soul trap spell in your main hand."
-      "\nStand in a safe location and make sure you have USLEEP disabled (if you use it) as it patches this method of training conjuration.")
+      "Move to a safe area and setup for whatever skill of magic you wish to train that requires casting.")
 
 
 try:
-    print('Press the "s" key to begin the casting soul trap.')
+    print('Press the "s" key to begin the casting the spel;.')
     keyboard.wait('s')
     print('Press Ctrl+C at any point when tabbed into the console window to quit the casting process.')
     print('Waiting 10 seconds then starting to send keystrokes. Please tab back into the game.')
     time.sleep(10)
     # Run the macro for "num_waits" iterations
-    for transmute_count in range(num_waits):
+    for _ in range(num_waits):
         # Cast the spell as many times as possible and then wait for an hour to regain magicka
-        for j in range(casts_per_wait):
+        for _ in range(casts_per_wait):
             time.sleep(0.25)
             mouse.press(Button.left)
             time.sleep(1)
